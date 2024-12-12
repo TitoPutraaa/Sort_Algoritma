54e27160f43d61cff9c900acb74ad3f90c56325a
Titoputraaa <titobamulo13@gmail.com>
2024-12-12 20:33:49 +0700

    start sort projek

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array = {9,6,3,2,7,8,4,1,5};
+
+
+    }
+
+    static void bubbleSort(int[] arr ){
+
+    }
+
+    static void selectionSort(int[] arr) {
+
+    }
+
+    static void insertionSort() {
+
+    }
+}
